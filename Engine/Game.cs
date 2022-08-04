50f9695adc675f8c1739824a3b38b1c89739d2d1
Io <luke114@outlook.com>
2022-08-04 17:17:35 -0600

    Co-authored-by: samquatrone <samquatrone@users.noreply.github.com>

--- Engine/Game.cs
+++ Engine/Game.cs
@@ -3,16 +3,26 @@ namespace Engine;
 public class Game {
     private Scene currentScene;
     private string? currentInput;
+    private bool done;
     public Game(Scene initalScene) {
         currentScene = initalScene;
+        done = false;
     }
     public void run() {
-        for (;;) {
+        initalize();
+        while (!done) {
             Console.WriteLine(currentScene.sceneDescription);
             currentInput = Console.ReadLine();
             // Game logic
             // if currentInput == north, Scene.north
             // 
         }
+        cleanUp();
+    }
+    private void initalize() {
+
+    }
+    private void cleanUp() {
+
     }
 }
